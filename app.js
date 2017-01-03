ac9be24634f127f56e23910f6ced499eed979c0b
Atanda <abdulsemiu.atanda@andela.com>
2017-01-03 10:22:56 +0100

    chore(configuration): Babel transpiler config

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser';
+import route from './server/routes';
 
 // Set up the express app
 const app = express();
+const port = process.env.PORT || 3000;
 
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./server/routes')(app);
+route(app);
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to Document management',
 }));
 
-app.listen('3000', () => {
-  console.log('App is running');
+app.listen(port, () => {
+  console.log(`App is running on ${port}`);
 });
