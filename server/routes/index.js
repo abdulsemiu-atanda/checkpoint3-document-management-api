74681dfca50e9b5adf21f9bc49dbab9dc57cb7d7
Atanda <abdulsemiu.atanda@andela.com>
2017-01-04 17:16:47 +0100

    feature(routing): add api endpoints

---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+import DocumentController from '../controllers/documents';
+import Role from '../controllers/role';
+import User from '../controllers/user';
+import Auth from '../controllers/auth';
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the Document Management API!',
+  }));
+  app.get('/api/role', Role.list);
+  app.get('/api/user/login', Auth.login);
+
+  app.post('/api/document', DocumentController.create);
+  app.post('/api/role', Role.create);
+  app.post('/api/user', User.create);
+
+  app.put('/api/user', User.update);
+};
