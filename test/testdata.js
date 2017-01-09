fb69dacbbe1e593e3b7e40ad4a491b1306a2b791
Atanda <abdulsemiu.atanda@andela.com>
2017-01-09 19:51:30 +0100

    feature(test): add test data

---
+++ test/testdata.js
@@ -0,0 +1,30 @@
+const testdata = {
+  fakeUser: {
+    firstName: 'Sam',
+    lastName: 'Winchester',
+    username: 'sam.winchester',
+    email: 'sam.winchester@gmail.com',
+    password: 'password',
+    roleId: 2
+  },
+  fakeAdmin: {
+    firstName: 'Shaquisha',
+    lastName: 'Daniels',
+    username: 'shaquisha.daniels',
+    email: 'shaquisha.daniels@gmail.com',
+    password: 'password',
+    roleId: 1
+  },
+  fakeUserDoc: {
+    title: 'Brown Fox',
+    content: 'The Quick Brown Fox jumps over the lazy dog',
+    access: 'public'
+  },
+  fakeAdminDoc: {
+    title: 'Scooby Doo',
+    content: 'Scooby Doo is an awesome dog who loves chewing bones',
+    access: 'private'
+  }
+};
+
+export default testdata;
