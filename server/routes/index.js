1a7272f8b7a4d3734ddc6e9fa03636378adc0bbe
Atanda <abdulsemiu.atanda@andela.com>
2017-01-10 15:30:28 +0100

    chore: remove redundant import statement

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,6 @@
 import DocumentController from '../controllers/documents';
 import Role from '../controllers/role';
 import User from '../controllers/user';
-import Auth from '../controllers/auth';
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
