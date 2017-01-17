79aae0d52bdd7648109e2ebe01aabe89f2f2e425
Atanda <abdulsemiu.atanda@andela.com>
2017-01-17 17:57:02 +0100

    update column validation

--- server/models/user.js
+++ server/models/user.js
@@ -12,10 +12,12 @@ module.exports = (sequelize, DataTypes) => {
     },
     email: {
       type: DataTypes.STRING,
+      unique: true,
       allowNull: false
     },
     username: {
       type: DataTypes.STRING,
+      unique: true,
       allowNull: false
     },
     password: {
