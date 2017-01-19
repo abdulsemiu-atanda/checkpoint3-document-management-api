91560df6f79882b96d69867f316c7ab739f85762
Atanda <abdulsemiu.atanda@andela.com>
2017-01-19 17:26:40 +0100

    update(route): add middleware to routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,27 +1,28 @@
 import Document from '../controllers/documents';
 import Role from '../controllers/role';
 import User from '../controllers/user';
+import Auth from '../controllers/auth';
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Document Management API!',
   }));
-  app.get('/api/role', Role.list);
-  app.get('/api/document', Document.list);
-  app.get('/api/document/:role', Document.access);
-  app.get('/api/user/:id/document', Document.fetchUserDoc);
-  app.get('/api/user', User.fetchDetails);
+  app.get('/api/role', Auth.adminUser, Role.list);
+  app.get('/api/document', Auth.validUser, Document.list);
+  app.get('/api/document/:role', Auth.adminUser, Document.access);
+  app.get('/api/user/:id/document', Auth.validUser, Document.fetchUserDoc);
+  app.get('/api/user', Auth.validUser, User.fetchDetails);
   app.get('/api/user/login', User.login);
   app.get('/api/user/logout', User.logout);
 
-  app.post('/api/document', Document.create);
-  app.post('/api/role', Role.create);
+  app.post('/api/document', Auth.validUser, Document.create);
+  app.post('/api/role', Auth.adminUser, Role.create);
   app.post('/api/user', User.create);
 
-  app.put('/api/user', User.update);
-  app.put('/api/document/:id', Document.update);
+  app.put('/api/user', Auth.validUser, User.update);
+  app.put('/api/document/:id', Auth.validUser, Document.update);
 
-  app.delete('/api/user', User.discard);
-  app.delete('/api/document', Document.discard);
-  app.delete('/api/role/:id', Role.discard);
+  app.delete('/api/user', Auth.adminUser, User.discard);
+  app.delete('/api/document', Auth.validUser, Document.discard);
+  app.delete('/api/role/:id', Auth.adminUser, Role.discard);
 };
